e291b5b8ea0371cfa7a184e1cc7195988e503d09
Gisela Torres <gis@Giselas-iMac.local>
2023-09-05 22:33:41 +0200

    first commit

---
+++ steps.sh
@@ -0,0 +1,76 @@
+# Variables
+RESOURCE_GROUP=tour-of-heroes-aca
+LOCATION=westeurope
+CONTAINERAPPS_ENVIRONMENT=tour-of-heroes-env
+
+# Add container app extension
+az extension add --name containerapp --upgrade
+
+# Register namespaces
+az provider register --namespace Microsoft.App
+az provider register --namespace Microsoft.OperationalInsights
+
+# Create a resource group
+az group create --name $RESOURCE_GROUP --location $LOCATION
+
+# Create a container app environment
+az containerapp env create \
+  --name $CONTAINERAPPS_ENVIRONMENT \
+  --resource-group $RESOURCE_GROUP \
+  --location "$LOCATION"
+
+
+# Deploy SQL Server database
+az containerapp create \
+  --name sqlserver \
+  --resource-group $RESOURCE_GROUP \
+  --environment $CONTAINERAPPS_ENVIRONMENT \
+  --image mcr.microsoft.com/mssql/server:latest \
+  --min-replicas 1 \
+  --max-replicas 1 \
+  --env-vars 'ACCEPT_EULA=Y' 'SA_PASSWORD=Password1!' 'MSSQL_PID=Express' \
+  --ingress internal \
+  --target-port 1433
+
+# List container apps
+az containerapp list \
+--resource-group $RESOURCE_GROUP \
+--environment $CONTAINERAPPS_ENVIRONMENT \
+-o table
+
+# Check SQL Server logs
+az containerapp logs show -n sqlserver -g $RESOURCE_GROUP
+
+# Deploy the API
+az containerapp create \
+  --name api \
+  --resource-group $RESOURCE_GROUP \
+  --environment $CONTAINERAPPS_ENVIRONMENT \
+  --image ghcr.io/0gis0/tour-of-heroes-dotnet-api/tour-of-heroes-api:0304097 \
+  --ingress external \
+  --target-port 5000 \
+  --env-vars 'ConnectionStrings__DefaultConnection=Server=tcp:sqlserver,1433;Initial Catalog=heroes;Persist Security Info=False;User ID=sa;Password=Password1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;' \
+  --allow-insecure true
+
+# Check API logs
+az containerapp logs show -n api -g $RESOURCE_GROUP
+
+# Attach the API container
+az containerapp exec -n api -g $RESOURCE_GROUP --command bash
+ls
+
+# Deploy Angular app
+az containerapp create \
+  --name angular \
+  --resource-group $RESOURCE_GROUP \
+  --environment $CONTAINERAPPS_ENVIRONMENT \
+  --image ghcr.io/0gis0/tour-of-heroes-angular/tour-of-heroes-angular:d39626e \
+  --min-replicas 1 \
+  --max-replicas 5 \
+  --ingress external \
+  --target-port 80 \
+  --exposed-port 80 \
+  --env-vars 'API_URL=http://api:5000'
+
+# Check frontend logs
+az containerapp logs show -n angular -g $RESOURCE_GROUP
