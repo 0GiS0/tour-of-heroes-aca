57c68192aec273094ac28a6c56d6739bf3378a58
0GiS0 <giselatb@outlook.com>
2023-11-30 12:34:48 +0100

    Add SQL Server and API deployment scripts

--- steps.sh
+++ steps.sh
@@ -1,7 +1,11 @@
 # Variables
-RESOURCE_GROUP=tour-of-heroes-aca
-LOCATION=westeurope
-CONTAINERAPPS_ENVIRONMENT=tour-of-heroes-env
+RESOURCE_GROUP="tour-of-heroes-aca"
+LOCATION="westeurope"
+CONTAINERAPPS_ENVIRONMENT="tour-of-heroes-env"
+
+SQL_SERVER_NAME="sqlserver-for-tour-of-heroes"
+SQL_USER_NAME="hero"
+SQL_PASSWORD="Password1"
 
 # Add container app extension
 az extension add --name containerapp --upgrade
@@ -9,6 +13,7 @@ az extension add --name containerapp --upgrade
 # Register namespaces
 az provider register --namespace Microsoft.App
 az provider register --namespace Microsoft.OperationalInsights
+az provider register --namespace Microsoft.ContainerService
 
 # Create a resource group
 az group create --name $RESOURCE_GROUP --location $LOCATION
@@ -17,35 +22,59 @@ az group create --name $RESOURCE_GROUP --location $LOCATION
 az containerapp env create \
   --name $CONTAINERAPPS_ENVIRONMENT \
   --resource-group $RESOURCE_GROUP \
-  --location "$LOCATION"
+  --location $LOCATION
 
+############# SQL Database #############
 
-# Deploy SQL Server database
-az containerapp create \
-  --name sqlserver \
+# Create SQL Server
+az sql server create \
+  --name $SQL_SERVER_NAME \
   --resource-group $RESOURCE_GROUP \
-  --environment $CONTAINERAPPS_ENVIRONMENT \
-  --image mcr.microsoft.com/mssql/server:latest \
-  --min-replicas 1 \
-  --max-replicas 1 \
-  --env-vars 'ACCEPT_EULA=Y' 'SA_PASSWORD=Password1!' 'MSSQL_PID=Express' \
-  --ingress internal \
-  --target-port 1433
+  --location $LOCATION \
+  --admin-user $SQL_USER_NAME \
+  --admin-password $SQL_PASSWORD
 
+#Enable firewall to access locally
+az sql server firewall-rule create \
+  --resource-group $RESOURCE_GROUP \
+  --server $SQL_SERVER_NAME \
+  --name AllowHomeIP \
+  --start-ip-address $(curl ifconfig.me) \
+  --end-ip-address $(curl ifconfig.me)
 
+# FQDN
+SQL_SERVER_FQDN=$(az sql server show \
+  --name $SQL_SERVER_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --query fullyQualifiedDomainName \
+  --output tsv)
 
-# Check SQL Server logs
-az containerapp logs show -n sqlserver -g $RESOURCE_GROUP
+echo $SQL_SERVER_FQDN
+
+# Allow Azure services to access the SQL Server
+az sql server firewall-rule create \
+  --resource-group $RESOURCE_GROUP \
+  --server $SQL_SERVER_NAME \
+  --name AllowAzureServices \
+  --start-ip-address 0.0.0.0 \
+  --end-ip-address 0.0.0.0
+
+# Check firewall rules
+az sql server firewall-rule list \
+  --resource-group $RESOURCE_GROUP \
+  --server $SQL_SERVER_NAME
 
 # Deploy the API
-az containerapp create \
+API_FQDN=$(az containerapp create \
   --name api \
   --resource-group $RESOURCE_GROUP \
   --environment $CONTAINERAPPS_ENVIRONMENT \
-  --image ghcr.io/0gis0/tour-of-heroes-dotnet-api/tour-of-heroes-api:f90ed80 \
+  --image ghcr.io/0gis0/tour-of-heroes-dotnet-api/tour-of-heroes-api:fd0c343 \
   --ingress external \
   --target-port 5000 \
-  --env-vars 'ConnectionStrings__DefaultConnection=Server=tcp:sqlserver,1433;Initial Catalog=heroes;Persist Security Info=False;User ID=sa;Password=Password1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;'
+  --env-vars "ConnectionStrings__DefaultConnection=Server=tcp:$SQL_SERVER_NAME.database.windows.net,1433;Initial Catalog=heroes;Persist Security Info=False;User ID=$SQL_USER_NAME;Password=$SQL_PASSWORD;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" \
+  --query "properties.configuration.ingress.fqdn" \
+  --output tsv)
 
 # Check API logs
 az containerapp logs show -n api -g $RESOURCE_GROUP
@@ -55,7 +84,7 @@ az containerapp exec -n api -g $RESOURCE_GROUP --command bash
 ls
 
 # Deploy Angular app
-az containerapp create \
+FRONTEND_FQDN=$(az containerapp create \
   --name frontend \
   --resource-group $RESOURCE_GROUP \
   --environment $CONTAINERAPPS_ENVIRONMENT \
@@ -65,7 +94,10 @@ az containerapp create \
   --ingress external \
   --target-port 80 \
   --exposed-port 80 \
-  --env-vars 'API_URL=http://api:5000'
+  --env-vars "API_URL=https://$API_FQDN/api/hero" --query "properties.configuration.ingress.fqdn" \
+  --output tsv)
+
+echo "https://$FRONTEND_FQDN"
 
 # Check frontend logs
 az containerapp logs show -n angular -g $RESOURCE_GROUP
@@ -75,3 +107,5 @@ az containerapp list \
 --resource-group $RESOURCE_GROUP \
 --environment $CONTAINERAPPS_ENVIRONMENT \
 -o table
+
+az group delete -n $RESOURCE_GROUP --yes
---
+++ steps_all_together.sh
@@ -0,0 +1,217 @@
+# Variables
+RESOURCE_GROUP="tour-of-heroes-aca"
+LOCATION="westeurope"
+CONTAINERAPPS_ENVIRONMENT="tour-of-heroes-env"
+VNET_NAME="heroes-vnet"
+KEYVAULT_NAME="heroes-kv"
+SQL_CONTAINER_APP_NAME="sqlserver"
+API_CONTAINER_APP_NAME="api"
+FRONTEND_CONTAINER_APP_NAME="frontend"
+
+# Add container app extension
+az extension add --name containerapp --upgrade
+
+# Register namespaces
+az provider register --namespace Microsoft.App --wait
+az provider register --namespace Microsoft.OperationalInsights --wait
+az provider register --namespace Microsoft.ContainerService --wait
+
+# Create a resource group
+az group create --name $RESOURCE_GROUP --location $LOCATION
+
+# Create a virtual network
+az network vnet create \
+  --name $VNET_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --location $LOCATION \
+  --address-prefixes 10.0.0.0/16 \
+  --subnet-name containers-subnet \
+  --subnet-prefixes 10.0.0.0/21
+
+# Get subnet ID
+SUBNET_ID=$(az network vnet subnet show \
+  --name containers-subnet \
+  --resource-group $RESOURCE_GROUP \
+  --vnet-name $VNET_NAME \
+  --query id \
+  --output tsv)
+
+# Delegate the subnet to Azure Container Apps
+az network vnet subnet update --ids $SUBNET_ID --delegations Microsoft.App/environments
+
+# Create Azure Key Vault
+az keyvault create \
+  --name $KEYVAULT_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --location $LOCATION
+
+# Create a secret for SQL Server SA password
+SA_PWD_ID=$(az keyvault secret set \
+  --vault-name $KEYVAULT_NAME \
+  --name "sqlserver-sa-password" \
+  --value "P@ssword123" \
+  --query id \
+  --output tsv)
+
+# Load connection string into Azure Key Vault using .env file
+SQL_SECRET_ID=$(az keyvault secret set \
+  --vault-name heroes-kv \
+  --name "sqlserver-connection-string" \
+  --file .env \
+  --query id \
+  --output tsv)
+
+# Create an identity for the SQL Server container
+SQL_IDENTITY_ID=$(az identity create \
+  --name sqlserver \
+  --resource-group $RESOURCE_GROUP \
+  --location $LOCATION \
+  --query id \
+  --output tsv)
+
+# Give permissions to the managed identity to read the secret
+az keyvault set-policy \
+  --name $KEYVAULT_NAME \
+  --object-id $(az identity show --id $SQL_IDENTITY_ID --query principalId --output tsv) \
+  --secret-permissions get
+
+# Create user-assigned managed identity 
+API_IDENTITY_ID=$(az identity create \
+  --name heroes-api \
+  --resource-group $RESOURCE_GROUP \
+  --location $LOCATION \
+  --query id \
+  --output tsv)
+
+# Give permissions to the managed identity to read the secret
+az keyvault set-policy \
+  --name heroes-kv \
+  --object-id $(az identity show --id $API_IDENTITY_ID --query principalId --output tsv) \
+  --secret-permissions get
+
+# Create a container app environment
+az containerapp env create \
+  --name $CONTAINERAPPS_ENVIRONMENT \
+  --resource-group $RESOURCE_GROUP \
+  --location $LOCATION \
+  --infrastructure-subnet-resource-id $SUBNET_ID
+
+# Create SQL Server container
+az containerapp create \
+  --name $SQL_CONTAINER_APP_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --environment $CONTAINERAPPS_ENVIRONMENT \
+  --image mcr.microsoft.com/mssql/server:latest \
+  --min-replicas 1 \
+  --max-replicas 1 \
+  --env-vars 'ACCEPT_EULA=Y' 'SA_PASSWORD=secretref:sa-pwd' \
+  --user-assigned $SQL_IDENTITY_ID \
+  --secrets "sa-pwd=keyvaultref:$SA_PWD_ID,identityref:$SQL_IDENTITY_ID" \
+  --ingress external \
+  --transport tcp \
+  --target-port 1433 \
+  --exposed-port 1433 \
+  --cpu 1.0 \
+  --memory 2.0Gi
+
+# Check SQL Server logs
+az containerapp logs show -n $SQL_CONTAINER_APP_NAME -g $RESOURCE_GROUP
+
+# Deploy the API
+API_FQDN=$(az containerapp create \
+  --name $API_CONTAINER_APP_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --environment $CONTAINERAPPS_ENVIRONMENT \  
+  --max-replicas 10 \
+  --image ghcr.io/0gis0/tour-of-heroes-dotnet-api/tour-of-heroes-api:fd0c343 \
+  --ingress external \
+  --target-port 5000 \
+  --scale-rule-name azure-http-rule \
+  --scale-rule-type http \
+  --scale-rule-http-concurrency 5 \
+  --user-assigned $API_IDENTITY_ID \
+  --secrets "connection-string=keyvaultref:$SQL_SECRET_ID,identityref:$API_IDENTITY_ID" \
+  --env-vars "ConnectionStrings__DefaultConnection=secretref:connection-string" \
+  --query "properties.configuration.ingress.fqdn" \
+  --output tsv)
+
+# Split your terminal and execute this command to watch how the replicas are scaled
+watch -n 5 az containerapp replica count --name $API_CONTAINER_APP_NAME --resource-group $RESOURCE_GROUP
+
+# Execute a load test
+brew install hey
+hey -z 60s -c 5 https://$API_FQDN
+
+# After load test finishes, wait for a while to see how replicas are scaled down
+# Check this doc about scale behaviour: https://learn.microsoft.com/en-us/azure/container-apps/scale-app?pivots=azure-cli#scale-behavior
+
+# Check API logs
+az containerapp logs show -n $API_CONTAINER_APP_NAME -g $RESOURCE_GROUP
+
+# Attach the API container
+az containerapp exec -n $API_CONTAINER_APP_NAME -g $RESOURCE_GROUP --command bash
+ls
+
+# Deploy Angular app
+FRONTEND_FQDN=$(az containerapp create \
+  --name frontend \
+  --resource-group $RESOURCE_GROUP \
+  --environment $CONTAINERAPPS_ENVIRONMENT \
+  --image ghcr.io/0gis0/tour-of-heroes-angular/tour-of-heroes-angular:d39626e \
+  --min-replicas 1 \
+  --ingress external \
+  --target-port 80 \
+  --exposed-port 80 \
+  --revisions-mode multiple \
+  --revision-suffix basic \
+  --env-vars "API_URL=https://$API_FQDN/api/hero" \
+  --query "properties.configuration.ingress.fqdn" \
+  --output tsv)
+
+echo "https://$FRONTEND_FQDN"
+
+# Create a new revision of the frontend
+FRONTEND_FQDN=$(az containerapp create \
+  --name frontend \
+  --resource-group $RESOURCE_GROUP \
+  --environment $CONTAINERAPPS_ENVIRONMENT \
+  --image ghcr.io/0gis0/tour-of-heroes-angular:heroes-with-pics \
+  --min-replicas 1 \  
+  --ingress external \
+  --target-port 80 \
+  --exposed-port 80 \
+  --env-vars "API_URL=https://$API_FQDN/api/hero" \
+  --revisions-mode multiple \
+  --revision-suffix pics \
+  --query "properties.configuration.ingress.fqdn" \
+  --output tsv)
+
+echo "https://$FRONTEND_FQDN"
+
+#give that revision a 'pics' label
+az containerapp revision label add \
+  --name $FRONTEND_CONTAINER_APP_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --label pics \
+  --revision frontend--pics
+
+# Traffic Split
+az containerapp ingress traffic set \
+  --name $FRONTEND_CONTAINER_APP_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --revision-weight frontend--basic=20 frontend--pics=80
+
+az containerapp ingress traffic show \
+  --name $FRONTEND_CONTAINER_APP_NAME \
+  --resource-group $RESOURCE_GROUP
+  
+# Check frontend logs
+az containerapp logs show -n $FRONTEND_CONTAINER_APP_NAME -g $RESOURCE_GROUP
+
+# List container apps
+az containerapp list \
+--resource-group $RESOURCE_GROUP \
+--environment $CONTAINERAPPS_ENVIRONMENT \
+-o table
+
+az group delete -n $RESOURCE_GROUP --yes
